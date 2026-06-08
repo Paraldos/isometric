26602de213c6b67dc53fcfe59fbda51324296878
Schiggy <hillbrecht.s@gmail.com>
2026-06-08 09:53:21 +0200

    refined bushes

--- deco/deco_template.gd
+++ deco/deco_template.gd
@@ -1,6 +1,17 @@
 extends Node2D
 
 @onready var guiding_block: Sprite2D = %GuidingBlock
+@onready var main_sprite: Sprite2D = $MainSprite
+@onready var main_animation_player: AnimationPlayer = %MainAnimationPlayer
+
+@export var randomize_sprite = false
+@export var animate_loop = false
 
 func _ready() -> void:
 	guiding_block.visible = false
+	if randomize_sprite:
+		main_sprite.frame = randi_range(0, main_sprite.hframes -1)
+	if main_animation_player.has_animation('loop') && animate_loop:
+		main_animation_player.play('loop')
+	elif main_animation_player.has_animation('RESET'):
+		main_animation_player.play('RESET')
--- deco/tree.gd
+++
@@ -1,7 +0,0 @@
-extends "res://deco/deco_template.gd"
-
-@onready var sprite_2d: Sprite2D = $Sprite2D
-
-func _ready() -> void:
-	super()
-	sprite_2d.frame = randi_range(0, sprite_2d.hframes -1)
