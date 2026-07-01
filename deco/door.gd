88c06cb6e9776a0f77e4d0a120c17caf777b5a26
Schiggy <hillbrecht.s@gmail.com>
2026-07-01 12:58:30 +0200

    refactored doors, door hinges and trapdoors

--- deco/door.gd
+++ deco/door.gd
@@ -3,8 +3,8 @@ extends Sprite2D
 enum HINGES {
 	NONE,
 	LEFT_GOLD,
-	LEFT_SILVER,
 	RIGHT_GOLD,
+	LEFT_SILVER,
 	RIGHT_SILVER,
 }
 @export var door_hinges : = HINGES.NONE
@@ -13,12 +13,6 @@ enum HINGES {
 
 func _ready() -> void:
 	hinges.visible = door_hinges != HINGES.NONE
-	match door_hinges:
-		HINGES.LEFT_SILVER:
-			hinges.frame = 2 if frame % 2 == 0 else 3
-		HINGES.LEFT_GOLD:
-			hinges.frame = 0 if frame % 2 == 0 else 1
-		HINGES.RIGHT_SILVER:
-			hinges.frame = 6 if frame % 2 == 0 else 7
-		HINGES.RIGHT_GOLD:
-			hinges.frame = 4 if frame % 2 == 0 else 5
+	hinges.frame = door_hinges - 1
+	if flip_h:
+		hinges.frame += 4
